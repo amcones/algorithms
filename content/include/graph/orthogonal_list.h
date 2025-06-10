900d748cfb8137e5fb52f7ddc0848fe8c31f368a
amcones <a587212@126.com>
2025-06-10 21:18:50 +0800

    feat: add graph docs
    Signed-off-by: amcones <a587212@126.com>

--- content/include/graph/orthogonal_list.h
+++ content/include/graph/orthogonal_list.h
@@ -5,7 +5,7 @@
  * Time: 2025-06-10 01:08:37 +0800
  */
 
-#ifndef CROSS_LINKLIST_H
-#define CROSS_LINKLIST_H
+#ifndef ORTHOGONAL_LIST_H
+#define ORTHOGONAL_LIST_H
 // TODO: add implementation
-#endif //CROSS_LINKLIST_H
+#endif //ORTHOGONAL_LIST_H
